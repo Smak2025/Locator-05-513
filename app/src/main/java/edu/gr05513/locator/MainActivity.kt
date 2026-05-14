55c32f60d632293206ca31ffdbf31a02b52323bc
Smak2025 <smak-80+2025@yandex.ru>
2026-05-14 17:04:10 +0300

    Отображение карты от Яндекса

--- app/src/main/java/edu/gr05513/locator/MainActivity.kt
+++ app/src/main/java/edu/gr05513/locator/MainActivity.kt
@@ -2,6 +2,7 @@ package edu.gr05513.locator
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -18,12 +19,21 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.app.ActivityCompat
+import com.yandex.mapkit.MapKitFactory
+import com.yandex.mapkit.geometry.Circle
+import com.yandex.mapkit.geometry.Point
+import com.yandex.mapkit.mapview.MapView
 import edu.gr05513.locator.locating.Locator
 import edu.gr05513.locator.ui.theme.LocatorTheme
 
@@ -36,7 +46,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        MapKitFactory.
+        MapKitFactory.setApiKey(BuildConfig.YANDEX_MAPS_API_KEY)
+        MapKitFactory.initialize(this)
 
         //2.
         requestPermissionLauncher = registerForActivityResult(
@@ -63,12 +74,13 @@ class MainActivity : ComponentActivity() {
                         ShowLocationAccessStatus(
                             vm.accessLocation,
                         )
-                        LazyColumn(modifier = Modifier.fillMaxSize()) {
+                        /*LazyColumn(modifier = Modifier.fillMaxSize()) {
                             items(vm.locations){
                                 Text(text = "Lat=${it.latitude}; Lon=${it.longitude}",
                                     modifier = Modifier.padding(8.dp))
                             }
-                        }
+                        }*/
+                        MapScreen()
                     }
                 }
             }
@@ -77,6 +89,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onStart() {
         super.onStart()
+        MapKitFactory.getInstance().onStart()
         // 3. Непосредственный запрос разрешений - в любом месте по необходимости
         requestPermissionLauncher.launch(
             arrayOf(
@@ -86,6 +99,10 @@ class MainActivity : ComponentActivity() {
         )
     }
 
+    override fun onStop() {
+        MapKitFactory.getInstance().onStop()
+        super.onStop()
+    }
 
     fun startLocating(){
         Locator.addLocationListener { location ->
@@ -129,4 +146,26 @@ fun ShowLocationAccessStatusPreview() {
     LocatorTheme {
         ShowLocationAccessStatus(false)
     }
+}
+
+@Composable
+fun MapScreen(
+    modifier: Modifier = Modifier,
+    currentLocation: Location? = null,
+){
+    var currentPoint by remember { mutableStateOf<Point?>(null) }
+    var yandexMap by remember { mutableStateOf<com.yandex.mapkit.map.Map?>(null) }
+    var locationCircle by remember { mutableStateOf<Circle?>(null) }
+
+    currentLocation?.let {
+        currentPoint = Point(currentLocation.latitude, currentLocation.longitude)
+    }
+
+    AndroidView(factory = { context ->
+        MapView(context).also{ view ->
+            view.mapWindow.map
+        }
+    }, update ={
+
+    })
 }
